7b23ac217142e4dc6ebd997c2833a14c1f96ca48
821675-rek <821675@manadulted.ac.uk>
2025-09-25 17:26:24 +0000

    Step 1 complete

--- Ex1_variables.py
+++ Ex1_variables.py
@@ -17,14 +17,14 @@ print(f"{food} is {name}'s favourite food!")
 # This will just add an empty line for space
 # Please don't touch this
 print("")
-bobName = 
-bobAge =
-bobFood =
-print("Hello, Bob!")
-print("Bob is 30 years old.")
-print("In 5 years, Bob will be 35 years old.")
-print("Bob really likes noodles.")
-print("Noodles is Bob's favourite food!")
+bobName = "Bob"
+bobAge = 30
+bobFood = "noodles"
+print(f"Hello, {bobName}!")
+print(f"{bobName} is {bobAge} years old.")
+print(f"In 5 years, Bob will be {bobAge + 5} years old.")
+print(f"{bobName} really likes {bobFood}.")
+print(f"{bobFood} is Bob's favourite food!")
 
 # Notice there's lots of repetition!
 # If we want to change Alice's age or favourite food, we'd have to change it in many places.
@@ -44,6 +44,20 @@ print("Noodles is Bob's favourite food!")
 
 # Step 3: Do the same for Bob, or make your own second person!
 # Use different values for name, age, and food.
+sandyName = "Sandy"
+sandyAge = 40
+sandyFood = "bread"
+print(f"Hello, {sandyName}!")
+print(f"{sandyName} is {sandyAge} years old.")
+print(f"In 5 years, {sandyName} will be {sandyAge + 5} years old.")
+print(f"{sandyName} really likes {sandyFood}.")
+print(f"{sandyFood} is Sandy's favourite food!")
+
+inputname = input("What is your name? ")
+inputage = int(input("How old are you? "))
+inputfood = input("What is your favourite food? ")
+print(f"Hello, {inputname}! Next year you will be {inputage + 1}.")
+print(f"{inputfood.capitalize()} is your favourite food!")
 
 # Step 4: Add a third person with their own data.
 # This should now be much quicker since you only need to change variables,
@@ -59,7 +73,6 @@ print("Noodles is Bob's favourite food!")
 # food = input("What is your favourite food? ")
 # print(f"Hello, {name}! Next year you will be {age + 1}.")
 # print(f"{food.capitalize()} is your favourite food!")
-
 # Once you are done, please run the following commands (one by one) in the terminal:
 # git add Ex1_variables.py
 # git commit -m "Completed variables exericse"
